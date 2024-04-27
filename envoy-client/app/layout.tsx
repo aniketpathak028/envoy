8f211f14f93e4cde57528ebb9ba0f34b5b57f286
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-04-27 13:37:18 +0530

    Minor change

--- envoy-client/app/layout.tsx
+++ envoy-client/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Envoy",
-  description: "",
+  description: "Email management tool to send, schedule, track E-mails",
 };
 
 export default function RootLayout({
